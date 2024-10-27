e0da6c75b7cec563cbabf09ccdc5f183de99c162
atnaryan9596 <ahamed.aryan9595@gmail.com>
2024-10-27 05:40:58 +0100

    Updated prompt for the AI model.

--- main.py
+++ main.py
@@ -1,66 +1,129 @@
 import os
 from groq import Groq
 import streamlit as st
+import json
 
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-# Streamlit placeholder with title then text input
+def parse_json(json_string):
+    try:
+        # Attempt to load the string as JSON
+        return json.loads(json_string)
+    except json.JSONDecodeError:
+        # If JSON decoding fails, display an error message
+        st.error("The JSON string is not formatted correctly.")
+        return None
+
 st.title("Dream Interpreter🌙", anchor=False)
-dream = st.text_area("Enter your dream below and I will interpret it for you. The more detailed the better.")
+dream = st.text_area("Enter your dream below and I will interpret it for you. The more detailed the better.", height=200, max_chars=2000)
 
 if st.button("💫 Interpret Dream 💫"):
     chat_completion = client.chat.completions.create(
-    #
-    # Required parameters
-    #
     messages=[
-        # Set an optional system message. This sets the behavior of the
-        # assistant and can be used to provide specific instructions for
-        # how it should behave throughout the conversation.
         {
             "role": "system",
-            "content": "You are a Dream Interpreter who is an expert at interpreting dreams. You will be given a dream as an input and you will interpret in as much as detail as possible. After that you will return a json format where the following Titles will be the keys and content as the values depending on the dream. Also use an appropriate emoji at the end of each title. Do not put values in a list structure. If there are multiple values just write them side by side with comma. Do not write anything outside of JSON. Make sure to use emojis after every values.\n\nDream Type: Could be Positive, Negative, Horrible, Disheartening, Joyful, Mysterious, Weird, Surreal, Lucid, Recurring etc.[Emoji]\n\nEmotion Intensity: Scale from 1-10 (how intense was the emotion)[Emoji]\n\nDominant Emotion: Fear, Happiness, Sadness, Excitement, Anger, Anxiety, Confusion, Surprise, Peacefulness[Emoji]\n\nVividness: Scale from 1-10 (how vivid or clear was the dream)[Emoji]\n\nReality Connection: Related to current life, Future-focused, Past memories, Fantasy, Alternate Reality[Emoji]\n\nSymbols or Themes: Common symbols (e.g., falling, flying, being chased, water, animals) Associated themes like success, conflict, or loss[Emoji]\n\nCharacters Involved: Family, Friends, Strangers, Animals, Celebrities, Unknown Entities[Emoji]\n\nSettings: Familiar place, Unknown location, Nature, Indoors, Cityscape, Fantasy land[Emoji]\n\nPotential Physical Reactions: Woke up sweating, Fast heartbeat, Heavy breathing, Calm wake-up[Emoji]\n\nPotential Lucidity Level: Lucid, Semi-Lucid, Non-Lucid[Emoji]\n\nDetailed Interpretation: [Give your interpretation here. Be as much accurate and detailed as possible. Try to understand what the user must have faced recently that caused the dream and try to relate it with recent events][No Emoji]",
+            "content": '''You are a Dream Interpreter who is an expert at interpreting dreams. You will be given a dream as an input and you will interpret in as much as detail as possible. After that you will return a json format where the following Titles will be the keys and content as the values depending on the dream. Also use an appropriate emoji at the end of each title. Do not put values in a list structure. If there are multiple values just write them side by side with comma. Make sure the json format is correct and ogranised. Do not write anything outside of JSON. Make sure to use emojis after every values.
+            {
+                    "Dream Type": "Surreal 🤯",
+                    "Emotion Intensity": "8/10 😨",
+                    "Dominant Emotion": "Fear 😨",
+                    "Vividness": "9/10 🔥",
+                    "Reality Connection": "Current Life Concerns 💔",
+                    "Symbols or Themes": "Falling, Loss of Control, Fear of Failure 🏗️",
+                    "Characters Involved": "Only myself 👥",
+                    "Settings": "Unfamiliar Building, Urban Cityscape 🏙️",
+                    "Potential Physical Reactions": "Woke up with a sudden jerk, Fast heartbeat 💥",
+                    "Potential Lucidity Level": "Non-Lucid 😴",
+                    "Detailed Interpretation": "It seems like you are experiencing a sense of losing control or feeling overwhelmed in your waking life..."
+            }''',
         },
-        # Set a user message for the assistant to respond to.
         {
             "role": "user",
             "content": dream,
         }
     ],
-
-    # The language model which will generate the completion.
+    
+    
     model="llama3-70b-8192",
-
-    #
-    # Optional parameters
-    #
-
-    # Controls randomness: lowering results in less random completions.
-    # As the temperature approaches zero, the model will become deterministic
-    # and repetitive.
     temperature=1,
-
-    # The maximum number of tokens to generate. Requests can use up to
+    # Requests can use up to
     # 32,768 tokens shared between prompt and completion.
     max_tokens=2048,
-
-    # Controls diversity via nucleus sampling: 0.5 means half of all
-    # likelihood-weighted options are considered.
     top_p=1,
-
-    # A stop sequence is a predefined or user-specified text string that
-    # signals an AI to stop generating content, ensuring its responses
-    # remain focused and concise. Examples include punctuation marks and
-    # markers like "[end]".
     stop=None,
-
-    # If set, partial message deltas will be sent.
     stream=False,
     )
+    
     dream_json = chat_completion.choices[0].message.content
     
-    st.write(dream_json)
+    parsed_data = parse_json(dream_json)
     
-    type(dream_json)
+    
+    if parsed_data:
+        st.title("🌟 Interpretation 🌟", anchor=False)
+
+        iterator = iter(parsed_data.items())
+
+        while True:
+            try:
+                key1, value1 = next(iterator)
+
+                try:
+                    key2, value2 = next(iterator)
+                except StopIteration:
+                    key2, value2 = None, None
+
+                if key2 is None:
+                    wide_col = st.columns(1)[0]
+                    with wide_col:
+                        st.subheader(key1, anchor=False)
+                        st.write(value1)
+                    break
+                else:
+                    col1, col2 = st.columns(2)
+
+                    with col1:
+                        st.subheader(key1, anchor=False)
+                        st.write(value1)
+
+                    with col2:
+                        st.subheader(key2, anchor=False)
+                        st.write(value2)
+
+            except StopIteration:
+                break
+    else:
+        st.write("Oops! The AI is tired and outputting gibberish. Please try again.")
+    
+    
+footer="""<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: white;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: 10px;
+}
+
+.footer p {
+    margin: 0;
+}
+</style>
+
+<div class="footer">
+    <p>Made with ❤ by Aryan</p>
+    <img src='https://groq.com/wp-content/uploads/2024/03/PBG-mark1-color.svg' alt='Powered by Groq for fast inference.' width='50' height='50'/>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
+            
